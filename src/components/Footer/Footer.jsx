f6f8c90cccc27d3276d00f571b625c1242698714
Shubham Salunke <90276117+Shubham-Salunke@users.noreply.github.com>
2024-06-20 23:48:44 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  
+  return (
+  <div className='footer'>
+    <p>Copyright @ 2024, Coin Dekho! - All Right Reserved.</p>
+  </div>
+  )
+}
+
+export default Footer
